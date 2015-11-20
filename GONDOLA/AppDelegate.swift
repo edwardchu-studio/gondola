101871a914744ad8a15b8a51cc31927083a8da93
储仲尧 <czy8023@me.com>
2015-11-20 19:27:35 +0800

    Initial Commit

---
+++ GONDOLA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GONDOLA
+//
+//  Created by 储仲尧 on 11/20/15.
+//  Copyright © 2015 UTOPIA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GONDOLA/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GONDOLA
+//
+//  Created by 储仲尧 on 11/20/15.
+//  Copyright © 2015 UTOPIA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
